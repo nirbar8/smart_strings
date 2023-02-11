bada395fe345d07d1266f26264235eee4a2db05a
Nir <banir@post.bgu.ac.il>
2023-02-11 07:49:54 -0500

    clean dir

--- Archive/mark_malicious.py
+++
@@ -1,7 +0,0 @@
-# script that mark all files as malicious
-for files in os.listdir('floss_output'):
-    with open(os.path.join('floss_output', files), 'r') as f:
-        data = json.load(f)
-    data['is_malicious'] = True
-    with open(os.path.join('floss_output', files), 'w') as f:
-        json.dump(data, f, indent=4)
